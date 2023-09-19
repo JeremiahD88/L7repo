0f323384e3fc89e289aa0ff6138e39296245ae8f
Raspberry <Jeremiahd88@gmail.com>
2023-09-19 11:44:36 +0200

    same shit

--- button.py
+++ button.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 
 import gpiozero
-import signal
+import time
 
 button = gpiozero.Button(20)
 
 def say_hello():
 	print('Hello')
-
+	
 button.when_pressed = say_hello
 
 
 while(1):
 	my_input = int(input('Bla'))	
-	print('Hejeh')
+	print('Hej')
 
 
 
